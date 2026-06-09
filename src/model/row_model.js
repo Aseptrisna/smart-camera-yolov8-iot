eacc269c3d018f8f39f5177587480357b518d040
A. Trisna Setiawan <aseptrisna1995@gmail.com>
2026-06-09 23:55:40 +0700

    bismillah jangan game over

--- src/model/row_model.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-// Alias for raw_image.model.js (kept for backward compatibility)
-module.exports = require('./raw_image.model');
--- src/service/rmq_service.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// RabbitMQ service is not used directly by the API.
-// The API only reads from MongoDB.
-// This file is a placeholder.
-
-module.exports = {};
